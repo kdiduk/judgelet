80727d2fd2f9d91416246682d4110c4b6a8faa3a
Kirill Diduk <kirill.diduk@gmail.com>
2025-10-06 00:15:42 +0200

    Connect to Redis and listen for events

--- judgelet/worker/__main__.py
+++ judgelet/worker/__main__.py
@@ -1,2 +1,19 @@
+import time
+import redis
+
+
 def main():
-    print("Hello from judgelet worker!")
+    r = redis.Redis()
+    try:
+        r.ping()
+    except redis.ConnectionError:
+        print("Could not connect to Redis server.")
+        return
+    print("Connected to Redis server.")
+
+    while True:
+        item = r.brpop("judgelet:tasks", timeout=5)
+        if not item:
+            time.sleep(0)
+        else:
+            print(f"Processing task: {item[1].decode()}")
